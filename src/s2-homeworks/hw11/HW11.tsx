d56e4c2fbe071b068dece0cde2bb4fa925786039
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-10-25 19:37:20 +0300

    complete 10 and 11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -15,12 +15,20 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: any, value: number[] | number) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+      // setValue1(value)
+      if (typeof value === 'object') {
+        setValue1(value[0])
+        setValue2(value[1])
+      } else {
+        setValue1(value)
+      }
+
     }
 
     return (
-        <div id={'hw11'}>
+        <div id={'hw11'} className={s2.hwWrapper}>
             <div className={s2.hwTitle}>Homework #11</div>
 
             <div className={s2.hw}>
@@ -29,6 +37,9 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            value={value1}
+                            onChange={change}
+                            color='secondary'
                             // сделать так чтоб value1 изменялось // пишет студент
 
                         />
@@ -37,6 +48,9 @@ function HW11() {
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            value={[value1, value2]}
+                            onChange={change}
+                            valueLabelDisplay="auto"
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
                         />
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import {alpha, Slider, SliderProps} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+  return (
+    <Slider
+      sx={{ // стили для слайдера // пишет студент
+        color: '#00CC22',
+        width: '150px',
+        '& .MuiSlider-rail' : {
+          color: '#8b8b8b',
+          opacity: '1'
+        },
+        '& .MuiSlider-thumb': {
+          width: '18px',
+          height: '18px',
+          border: '6px solid #fff',
+          outline: '1px solid #00CC22',
+          '&:hover, &.Mui-focusVisible': {
+            boxShadow: `0px 0px 0px 6px ${alpha('#00CC22', 0.16)}`,
+          },
+          '&.Mui-active': {
+            boxShadow: `0px 0px 0px 9px ${alpha('#00CC22', 0.16)}`,
+          },
+
+        },
+
+
+      }}
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  )
 }
 
 export default SuperRange
